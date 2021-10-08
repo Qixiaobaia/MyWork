5932cbd004d5e140e2685347d66b5462d3d5a7ba
Qixiaobaia <1640174137@qq.com>
2021-10-08 22:17:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mywork_lcf/weixin3.java
@@ -0,0 +1,23 @@
+package com.example.mywork_lcf;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.app.Fragment;
+
+public class weixin3 extends Fragment {
+
+
+    public weixin3() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weixin3, container, false);
+    }
+}
